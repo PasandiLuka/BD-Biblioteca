c01db92ee06b0f192078d2abbda49c4c45b6b7b4
PasandiLuka <lukapasandi@gmail.com>
2024-10-31 20:08:00 -0300

    first commit

---
+++ 00 DDL.sql
@@ -0,0 +1,20 @@
+-- Active: 1730050857382@@127.0.0.1@3306@bd_biblioteca
+DROP DATABASE IF EXISTS bd_biblioteca;
+CREATE DATABASE bd_biblioteca;
+USE bd_biblioteca;
+/*
+Genero{
+
+
+    tinyIntUnsigned idGenero PK
+    varchar(45) genero
+
+
+}
+
+*/
+CREATE TABLE(
+    TINYINT UNSIGNED idGenero
+
+
+)
