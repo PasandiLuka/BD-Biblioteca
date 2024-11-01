f47a6d20b4b7607abe76f7efc96d178e0745e90c
PasandiLuka <lukapasandi@gmail.com>
2024-11-01 11:01:38 -0300

    Agrege NOT NULL

--- 00 DDL.sql
+++ 00 DDL.sql
@@ -4,32 +4,41 @@ USE bd_biblioteca;
 
 CREATE TABLE Genero(
     idGenero TINYINT UNSIGNED,
-    genero VARCHAR(45),
+    genero VARCHAR(45) NOT NULL,
     CONSTRAINT PK_Genero PRIMARY KEY (idGenero)
 );
 
 CREATE TABLE Bibliotecario(
     idBibliotecario INT UNSIGNED,
-    inicioHoraLaboral DATETIME,
-    finHoraLaboral DATETIME,
+    nombre VARCHAR(45) NOT NULL,
+    inicioHoraLaboral TIME NOT NULL,
+    finHoraLaboral TIME NOT NULL,
     CONSTRAINT PK_Genero PRIMARY KEY (idBibliotecario)
 );
 
 CREATE TABLE Autor(
     idAutor INT UNSIGNED,
-    bibliografia VARCHAR(500),
-    nacimiento DATETIME,
-    fallecimiento DATETIME,
+    nombre VARCHAR(45) NOT NULL,
+    bibliografia VARCHAR(500) NOT NULL,
+    nacimiento DATE NOT NULL,
+    fallecimiento DATE,
     CONSTRAINT PK_Genero PRIMARY KEY (idAutor)
+);
+
+CREATE TABLE Cliente(
+    DNI INT UNSIGNED,
+    nombre VARCHAR(45) NOT NULL,
+    apellido VARCHAR(45) NOT NULL,
+    CONSTRAINT PK_Cliente PRIMARY KEY (DNI)
 );
 
 CREATE TABLE Libro(
     ISBN INT UNSIGNED,
     idAutor INT UNSIGNED,
     idGenero TINYINT UNSIGNED,
-    titulo VARCHAR(45),
-    publicacion DATE,
-    calificacion TINYINT UNSIGNED,
+    titulo VARCHAR(45) NOT NULL,
+    publicacion DATE NOT NULL,
+    calificacion TINYINT UNSIGNED NOT NULL,
     CONSTRAINT PK_Libro PRIMARY KEY (ISBN),
     CONSTRAINT FK_Libro_Autor FOREIGN KEY (idAutor)
         REFERENCES Autor (idAutor),
@@ -41,8 +50,8 @@ CREATE TABLE Prestamo(
     idPrestamo INT UNSIGNED,
     idBibliotecario INT UNSIGNED,
     ISBN INT UNSIGNED,
-    fechaEntrega DATE,
-    fechaDevolucion DATE,
+    fechaEntrega DATE NOT NULL,
+    fechaDevolucion DATE NOT NULL,
     CONSTRAINT PK_Prestamo PRIMARY KEY (idPrestamo),
     CONSTRAINT FK_Prestamo_Bibliotecario FOREIGN KEY (idBibliotecario)
         REFERENCES Bibliotecario (idBibliotecario),
@@ -50,19 +59,12 @@ CREATE TABLE Prestamo(
         REFERENCES Libro (ISBN)
 );
 
-CREATE TABLE Cliente(
-    DNI INT UNSIGNED,
-    nombre VARCHAR(45),
-    apellido VARCHAR(45),
-    CONSTRAINT PK_Cliente PRIMARY KEY (DNI)
-);
-
 CREATE TABLE Sancion(
     idSancion INT UNSIGNED,
     idBibliotecario INT UNSIGNED,
     DNI INT UNSIGNED,
-    fechaEmision DATE, 
-    multa DECIMAL(10,2),
+    fechaEmision DATE NOT NULL, 
+    multa DECIMAL(10,2) NOT NULL,
     CONSTRAINT PK_Genero PRIMARY KEY (idSancion),
     CONSTRAINT FK_Sancion_Cliente FOREIGN KEY (idBibliotecario) 
         REFERENCES Bibliotecario (idBibliotecario)
