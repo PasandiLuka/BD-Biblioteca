482fc12242a20832d45373a2ff70fbb9c217c074
PasandiLuka <lukapasandi@gmail.com>
2025-06-30 16:15:29 -0300

    Correccion

--- 00 DDL.sql
+++ 00 DDL.sql
@@ -80,17 +80,7 @@ CREATE TABLE Sancion(
 
 CREATE TABLE Calificacion(
     idCalificacion INT UNSIGNED,
-    ISBN INT UNSIGNDELIMITER $$
-DROP TRIGGER IF EXISTS aftInsCalificacion $$
-CREATE TRIGGER aftInsCalificacion AFTER INSERT ON Calificacion
-FOR EACH ROW
-BEGIN
-    UPDATE Libro
-    SET calificacion = (SELECT AVG(calificacion)
-                       FROM Calificacion
-                       WHERE ISBN = NEW.ISBN)
-    WHERE ISBN = NEW.ISBN;
-END $$ED,
+    ISBN INT UNSIGNED,
     DNI INT UNSIGNED,
     calificacion TINYINT UNSIGNED,
     CONSTRAINT PK_Calificacion PRIMARY KEY (idCalificacion),
