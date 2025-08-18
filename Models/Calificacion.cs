c4685a050a0394a0745633c76f79156595c1829c
PasandiLuka <lukapasandi@gmail.com>
2025-08-18 15:28:48 -0300

    xd
    xd

--- Models/Calificacion.cs
+++
@@ -1,7 +0,0 @@
-public class Calificacion
-{
-    public uint IdCalificacion { get; set; }    
-    public uint ISBN { get; set; }    
-    public uint DNI { get; set; }    
-    public decimal ValorCalificacion { get; set; }
-}
--- Models/Sancion.cs
+++
@@ -1,9 +0,0 @@
-public class Sancion
-{
-    public uint IdSancion { get; set; }    
-    public uint IdBibliotecario { get; set; }    
-    public uint IdPrestamo { get; set; }    
-    public DateTime FechaEmision { get; set; }
-    public decimal Multa { get; set; }
-}
-
