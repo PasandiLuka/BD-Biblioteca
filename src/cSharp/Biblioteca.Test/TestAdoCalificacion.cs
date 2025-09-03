47feef98f0c3ba42bfce4a79ee9f6062ef2e0edf
Diego-2810 <diegoemilianoalganaras@gmail.com>
2025-09-03 15:44:30 -0300

    Test Calificacion

--- src/cSharp/Biblioteca.Test/TestAdoCalificacion.cs
+++ src/cSharp/Biblioteca.Test/TestAdoCalificacion.cs
@@ -17,11 +17,42 @@ namespace Biblioteca.Test
             _conexion = new MySqlConnection(cadenaConexion);
             _repoLibro = new RepoLibro(_conexion);
         }
-        
+
         [Fact]
         public void CuandoHaceUnInsertEnCalificacion_DebeAlmacenarDichaFilaEnLaTablaCalifcacion()
         {
-            
+            var _calificacion = new Calificacion()
+            {
+                idCalificacion = 1,
+                ISBN = 1,
+                DNI = 1,
+                calificacion = 1
+
+            };
+
+            _repoLibro.AltaCalificacion(_calificacion);
+
+            var detalleCalificacion = _repoLibro.DetalleCalificacion(1);
+
+            Assert.NotNull(detalleCalificacion);
+            Assert.Equal<uint>(1, detalleCalificacion.idCalificacion);
+            Assert.Equal<uint>(1, detalleCalificacion.ISBN);
+            Assert.Equal<uint>(1, detalleCalificacion.DNI);
+            Assert.Equal(1, detalleCalificacion.calificacion);
+        }
+
+        [Fact]
+        public void CuandoRealizoUnInsertEnCalificacionConUnaPkDuplicada_DebeDevolverLaExcepcion()
+        {
+           var _calificacion = new Calificacion()
+            {
+                idCalificacion = 1,
+                calificacion = 1,
+                ISBN = 1,
+                DNI = 1
+                
+            };
+        Assert.Throws<MySqlException>(() => _repoLibro.AltaCalificacion(_calificacion));
         }
     }
 }
